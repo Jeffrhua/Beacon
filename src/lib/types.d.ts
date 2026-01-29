c43164e56bf57dd92cace23f3ad89a7216f5dd64
Chris-Kau <uhchris04@gmail.com>
2026-01-29 00:47:33 -0800

    added owner_id to group types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,24 +1,26 @@
 import type { ObjectId } from "mongodb";
 export type GroupDb = {
-  _id: ObjectId;
-  title: string;
-  description: string;
+  _id: ObjectId,
+  title: string,
+  description: string,
+  owner_id: ObjectId
 };
 
 export type Group = {
     id: string,
     title: string,
-    description: string
+    description: string,
+    owner_id: string
 }
 
 export type UserDb = {
-  _id: ObjectId;
-  name: string;
+  _id: ObjectId,
+  name: string,
   email: string
 }
 
 export type User = {
-  id: string;
-  name: string;
+  id: string,
+  name: string,
   email: string
 }

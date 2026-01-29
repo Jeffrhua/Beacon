651b7122403f01c671ad63194c55dcb0b6d56435
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-01-29 11:57:05 -0800

    added NavBar and SideBar components

---
+++ src/routes/(protected)/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Sidebar from "$lib/components/Sidebar.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+	let { children } = $props();
+    let sidebarOpen = $state(true);
+    function toggleSidebar(){
+        sidebarOpen = !sidebarOpen;
+    }
+
+    
+</script>
+
+<div class="app">
+    <Navbar toggleSidebar={toggleSidebar} title="Dashboard"></Navbar>
+    <Sidebar sidebarOpen={sidebarOpen}></Sidebar>
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+</style>

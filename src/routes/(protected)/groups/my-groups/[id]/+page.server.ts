c9c90598e2f9252c63e527bd76f7baf08f901f4a
Chris-Kau <uhchris04@gmail.com>
2026-01-29 00:48:06 -0800

    fetched users and added users and owner to the return statement

--- src/routes/(protected)/groups/my-groups/[id]/+page.server.ts
+++ src/routes/(protected)/groups/my-groups/[id]/+page.server.ts
@@ -1,15 +1,31 @@
-import { getGroup } from '$lib/server/mongodb.js'
+import { getGroup, getGroupUsers, getUser } from '$lib/server/mongodb.js'
 import type { GroupDb } from '$lib/types.js';
 import { ObjectId } from 'mongodb';
 export const load = async ({params}) => {
     const groupId = new ObjectId(params.id);
     const group = await getGroup(groupId);
+    const groupUsers = await getGroupUsers(groupId);
     if(group == null){
         return {error: "Group not found"}
     }
-
-    const {_id, ...r} = group;
-
-
-    return {group: {id: _id.toString(), ...r}}
+    const ownerId = group.owner_id
+    const owner = await getUser(ownerId);
+    console.log(owner)
+    const ownerSerialized = owner
+        ? {
+            id: owner._id.toString(),
+            name: owner.name,
+            displayName: owner.displayName
+        }
+        : null;
+    let users = [];
+    groupUsers.forEach((u)=>{
+        users.push({id: u._id.toString(), name: u.name, displayName: u.displayName})
+    })
+    const {_id, owner_id, ...r} = group;
+    return {
+        group: {id: _id.toString(), ...r},
+        users: users,
+        owner: ownerSerialized
+    }
 }

821bc4eef00d6bd10b7dc56a06ec0a81ae798f9c
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-02-03 14:56:10 -0800

    added Notifcation drop down

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { page } from "$app/state";
 	import { HomeSolid, BellSolid } from 'flowbite-svelte-icons';
-    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { Button, Dropdown, DropdownItem, ListgroupItem } from "flowbite-svelte";
     import { Card, Listgroup, Avatar } from "flowbite-svelte";
     let {toggleSidebar, title, notifications = []} = $props();
     let activeUrl = $derived(page.url.pathname);
@@ -20,11 +20,27 @@
     <div class="px-10">
         <Button outline color="dark"><BellSolid class="shrink-0 h-7 w-7 cursor-pointer dark:text-white hover:text-gray-700"/></Button>
         <Dropdown activeUrl={activeUrl}>
-            <Listgroup items={notifications} class="border-0 dark:bg-transparent!">
+            <Listgroup items={notifications} class="border-0 dark:bg-transparent! w-[300px]">
                 {#snippet children(item)}
-                <div class="flex items-center space-x-4 py-2 rtl:space-x-reverse">
-                    <DropdownItem>{item.title}</DropdownItem>
-                </div>
+                    <ListgroupItem href="/groups/{item.groupId}">
+                        <div class="min-w-full w-full flex items-center space-x-4 py-2 rtl:space-x-reverse">
+                            <div class="min-w-0 max-w-[260px]">
+                                <p class="truncate text-sm font-medium text-gray-900 dark:text-white">
+                                    {item.groupName}
+                                </p>
+                                <p class="truncate text-sm font-small text-gray-900 dark:text-white">
+                                    {item.alertTitle}
+                                </p>
+                                <p class="truncate text-sm text-gray-500 dark:text-gray-400">
+                                    {item.alertCreated}
+                                </p>
+                            </div>
+
+                            <div class="ml-auto shrink-0 text-base font-semibold text-gray-900 dark:text-white">
+                                {item.alertSeverity}
+                            </div>
+                        </div>
+                    </ListgroupItem>
                 {/snippet}
             </Listgroup>
         </Dropdown>

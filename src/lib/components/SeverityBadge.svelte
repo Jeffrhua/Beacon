18b4167076957db7f53917c3ec1f2e0e5f97c6bf
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-02-03 15:42:11 -0800

    created badge component

---
+++ src/lib/components/SeverityBadge.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Badge } from "flowbite-svelte";
+  let {severity="low"} = $props();
+</script>
+<div>
+    {#if severity == "low"}
+        <Badge color="green">Low</Badge>
+    {:else if severity == "medium"}
+        <Badge color="yellow">Medium</Badge>
+    {:else if severity == "high"}
+        <Badge class="bg-[#f67e7e] text-[#822a2a]">High</Badge>
+    {:else if severity == "critical"}
+        <Badge class="bg-[#9e0000] text-white">Critical</Badge>
+    {/if}
+</div>
+

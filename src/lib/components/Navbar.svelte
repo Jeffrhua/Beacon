731302d5de406e50d5be2d36a1f6ecfbc6cd411c
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-02-03 12:17:11 -0800

    made basic dropdown menu for notifications

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    let {toggleSidebar, title, notifications = []} = $props();
+    import { page } from "$app/state";
 	import { HomeSolid, BellSolid } from 'flowbite-svelte-icons';
-
+    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { Card, Listgroup, Avatar } from "flowbite-svelte";
+    let {toggleSidebar, title, notifications = []} = $props();
+    let activeUrl = $derived(page.url.pathname);
 </script>
 
 <div class="w-full flex items-center justify-between px-4 py-3 border-b bg-gray-50">
@@ -14,7 +17,16 @@
             {title}
         </h1>
     </div>
-    <div>
-        <BellSolid class="shrink-0 h-7 w-7 cursor-pointer dark:text-white hover:text-gray-700 "/>
+    <div class="px-10">
+        <Button outline color="dark"><BellSolid class="shrink-0 h-7 w-7 cursor-pointer dark:text-white hover:text-gray-700"/></Button>
+        <Dropdown activeUrl={activeUrl}>
+            <Listgroup items={notifications} class="border-0 dark:bg-transparent!">
+                {#snippet children(item)}
+                <div class="flex items-center space-x-4 py-2 rtl:space-x-reverse">
+                    <DropdownItem>{item.title}</DropdownItem>
+                </div>
+                {/snippet}
+            </Listgroup>
+        </Dropdown>
     </div>
 </div>

4f58de4843a3fa952db1d1db52282e95b49d1659
Chris-Kau <uhchris04@gmail.com>
2026-02-03 18:57:02 -0800

    fixed spacing issue

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,20 +8,20 @@
     let activeUrl = $derived(page.url.pathname);
 </script>
 
-<div class="w-full flex items-center justify-between px-4 py-3 bg-gray-50">
+<div class="w-full h-full flex items-center justify-between px-4 py-3 bg-gray-50 dark:bg-[#23272F]">
     <button class="p-2 hover:bg-gray-200 rounded" onclick={toggleSidebar} aria-label="Toggle menu">
 		☰
 	</button>
     <div class ="flex items-center gap-2 w-full justify-center">
-        <HomeSolid class="shrink-0 h-6 w-6" />
-        <h1 class="text-lg font-semibold">
+        <HomeSolid class="shrink-0 h-6 w-6 dark:text-gray-50 text-black" />
+        <h1 class="text-lg font-semibold dark:text-gray-50 text-black">
             {title}
         </h1>
     </div>
     <div class="px-10">
         <Button outline color="dark"><BellSolid class="shrink-0 h-7 w-7 cursor-pointer dark:text-white"/></Button>
         <Dropdown activeUrl={activeUrl}>
-            <Listgroup items={notifications} class="border-0 dark:bg-transparent! w-[300px]">
+            <Listgroup items={notifications} class="border-0 dark:bg-transparent w-[300px]">
                 {#snippet children(item)}
                     <ListgroupItem href="/groups/{item.groupId}">
                         <div class="min-w-full w-full flex items-center space-x-4 py-2 rtl:space-x-reverse">

0dec1209381cb08f6faa078cc740ff7355210e75
Chris-Kau <uhchris04@gmail.com>
2026-02-03 18:57:20 -0800

    uses local storage now

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     import Sidebar from "$lib/components/Sidebar.svelte";
     import Navbar from "$lib/components/Navbar.svelte";
-    import { themeState } from "$lib/states/theme.svelte";
-	let { children, data } = $props();
+	import {browser} from "$app/environment";
+    import { onMount } from "svelte";
+    import "../../app.css"
+    let { children, data } = $props();
+    
     let notifcations = data.userAlerts.length !== 0 ? data.userAlerts : [];
     let sidebarOpen = $state(true);
-
+    let theme = $state("light");
+    onMount(()=>{
+        if( browser ){
+	        theme = localStorage.getItem('theme') || 'light';
+        }
+    })
     function toggleSidebar(){
         sidebarOpen = !sidebarOpen;
     }
@@ -13,7 +21,7 @@
     
 </script>
 
-<div class="{themeState.value} app h-screen grid grid-rows-[64px_1fr] dark:bg-[#1C1E22] bg-[#FFFFFF] {sidebarOpen ? "grid-cols-[240px_1fr]" : "grid-cols-1"}">
+<div class="{theme} app h-screen grid grid-rows-[64px_1fr] bg-[#FFFFFF] {sidebarOpen ? "grid-cols-[240px_1fr]" : "grid-cols-1"}">
     <header class="{sidebarOpen ? "col-span-2" : "col-span-1"} z-50">
         <Navbar toggleSidebar={toggleSidebar} notifications={notifcations} title="Dashboard"></Navbar>
     </header>
@@ -22,10 +30,9 @@
             <Sidebar></Sidebar>
         </aside>
     {/if}
-	<main class="overflow-y-auto p-4">
+	<main class="overflow-y-auto p-4 ">
 		{@render children()}
 	</main>
 </div>
 
-<style>
-</style>
+

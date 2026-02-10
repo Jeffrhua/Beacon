eb8cfb08e7965d52edb8aaa763de886d821a5397
Nyrin Thai <159681242+nyrinthai@users.noreply.github.com>
2026-02-10 11:26:00 -0800

    added pagination

--- src/routes/(protected)/groups/group-discovery/+page.server.ts
+++ src/routes/(protected)/groups/group-discovery/+page.server.ts
@@ -1,14 +1,30 @@
 import { getAllGroups } from "$lib/server/mongodb";
 import type { Group, GroupDb } from "$lib/types.js";
+import { test } from "vitest";
+import { ObjectId } from "mongodb";
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const length = randomInt(50, 100);
+
+const testGroupsDb = Array.from({ length }, (_, i) => ({
+  _id: new ObjectId(),
+  title: `Test Group ${i + 1}`,
+  description: `Test description for group ${i + 1}`,
+  owner_id: new ObjectId()
+}));
 
 export const load = async () => {
-    const groups : GroupDb[] = await getAllGroups();
-
+    //const groups : GroupDb[] = await getAllGroups();
+    const groups = testGroupsDb; // For testing purposes only
     const serialized: Group[] = groups.map(({_id, owner_id, ...r}) =>({
         id: _id.toString(), 
         owner_: owner_id.toString(),
         ...r
     }))
+
 
 
     return {

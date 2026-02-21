9a59596d5502a566be9adecb03a195a1a33394c5
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-02-20 20:25:49 -0800

    can now export group alerts to pdf

--- src/routes/(protected)/groups/[id]/+page.svelte
+++ src/routes/(protected)/groups/[id]/+page.svelte
@@ -1,27 +1,55 @@
 <script lang="ts">
-    import {page} from '$app/state';
-    import {Card, Listgroup, Button, Group} from "flowbite-svelte";
-    import {UserOutline} from "flowbite-svelte-icons";
-    import SendAlertModal from '$lib/components/SendAlertModal.svelte';
-    import SeverityBadge from '$lib/components/SeverityBadge.svelte';
-    import JoinGroupBtn from '$lib/components/JoinGroupBtn.svelte';
-    import RemoveGroupBtn from '$lib/components/RemoveGroupBtn.svelte';
-    import DeleteGroupBtn from '$lib/components/DeleteGroupBtn.svelte';
-    import GroupSettings from '$lib/components/GroupSettings.svelte';
+    import { page } from "$app/state";
+    import { Card, Listgroup, Button, Group } from "flowbite-svelte";
+    import { UserOutline } from "flowbite-svelte-icons";
+    import { formatDate } from "$lib/utils.js";
+    import { jsPDF } from "jspdf";
+    import { autoTable } from "jspdf-autotable";
+    import SendAlertModal from "$lib/components/SendAlertModal.svelte";
+    import SeverityBadge from "$lib/components/SeverityBadge.svelte";
+    import JoinGroupBtn from "$lib/components/JoinGroupBtn.svelte";
+    import RemoveGroupBtn from "$lib/components/RemoveGroupBtn.svelte";
+    import DeleteGroupBtn from "$lib/components/DeleteGroupBtn.svelte";
+    import GroupSettings from "$lib/components/GroupSettings.svelte";
 
     let { id } = page.params;
     let { data } = $props();
     let formModal = $state(false);
     let deleteGroupForm = $state(false);
     let settingsModal = $state(false);
-    let alerts = data.alerts ? data.alerts : []
+    let alerts = data.alerts ? data.alerts : [];
     const isOwner = data.owner?.id === data.currentUser;
     const isAdmin = data.isAdmin;
 
-    console.log(data.owner?.id)
+    const exportAlertsToPDF = () => {
+        if(!data.group) return;
+        const columns = ["Alert Id", "Title", "Description", "Severity", "Long", "Lat", "Address", "Sent By", "Date Created"]
+        const rows = alerts.map((a) => [
+            a.id.toString(),
+            a.title.toString(),
+            a.description.toString(),
+            a.severity.toString(),
+            a.longitude.toString(),
+            a.latitude.toString(),
+            a.address.toString(),
+            a.user_id.toString(),
+            formatDate(a.dateCreated)
+        ]);
+        var doc = new jsPDF({ orientation: "landscape" });
+        doc.text(`Alert Report for ${data.group.title} (${data.group.id})`,10,10)
+        autoTable(doc, {
+            head: [columns],
+            body: rows,
+            styles: {
+                fontSize: 8,
+            }
+        });
+        doc.save(`${data.group.title}_${data.group.id}_Report_${formatDate(new Date())}`)
+    };
 </script>
 
 <div class="grid h-full grid-cols-[75%_1fr] grid-rows-1 gap-2">
+    <!-- Main Body -->
     <div>
         {#if data.group}
             <h1 class="text-2xl">{data.group.title}</h1>
@@ -34,29 +62,53 @@
         <!-- Info Card -->
         <Card class="p-4 w-full max-w-none">
             <div class="flex items-center">
-                <h5 class="text-xl leading-none font-bold text-gray-900 dark:text-white">Info</h5>
+                <h5
+                    class="text-xl leading-none font-bold text-gray-900 dark:text-white"
+                >
+                    Info
+                </h5>
                 <div class="ml-auto flex items-center">
-                    <UserOutline class="h-5 w-5 translate-x-[-5px] text-gray-900 dark:text-white"/> 
-                    <p class="text-gray-900 dark:text-white">{data.users.length}</p>
+                    <UserOutline
+                        class="h-5 w-5 translate-x-[-5px] text-gray-900 dark:text-white"
+                    />
+                    <p class="text-gray-900 dark:text-white">
+                        {data.users.length}
+                    </p>
                 </div>
-
             </div>
 
             <div>
-                <h6 class="text-md font-semibold text-gray-900 dark:text-white">Owner:</h6>
+                <h6 class="text-md font-semibold text-gray-900 dark:text-white">
+                    Owner:
+                </h6>
                 {#if data.owner}
-                    <p class="text-gray-900 dark:text-white">{data.owner.displayName ? data.owner.displayName : data.owner.name}</p>
+                    <p class="text-gray-900 dark:text-white">
+                        {data.owner.displayName
+                            ? data.owner.displayName
+                            : data.owner.name}
+                    </p>
                 {/if}
             </div>
             <div>
-                <h6 class="text-md font-semibold text-gray-900 dark:text-white">Members:</h6>
-                
+                <h6 class="text-md font-semibold text-gray-900 dark:text-white">
+                    Members:
+                </h6>
+
                 {#if data.users}
-                    <Listgroup items={data.users} class="border-0 dark:bg-transparent">
+                    <Listgroup
+                        items={data.users}
+                        class="border-0 dark:bg-transparent"
+                    >
                         {#snippet children(user)}
                             {#if user.id != data.owner?.id}
-                                <div class = "flex items-center py-2">
-                                    <p class="text-sm font-sm text-gray-900 dark:text-white">{user.displayName ? user.displayName : user.name}</p>
+                                <div class="flex items-center py-2">
+                                    <p
+                                        class="text-sm font-sm text-gray-900 dark:text-white"
+                                    >
+                                        {user.displayName
+                                            ? user.displayName
+                                            : user.name}
+                                    </p>
                                 </div>
                             {/if}
                         {/snippet}
@@ -68,19 +120,38 @@
         <!-- Alerts Display Area -->
         <Card class="p-4 w-full flex-1 max-w-none">
             <div class="mb-4 flex items-center justify-between">
-                <h5 class="text-xl leading-none font-bold text-gray-900 dark:text-white">Latest Alerts</h5>
+                <h5
+                    class="text-xl leading-none font-bold text-gray-900 dark:text-white"
+                >
+                    Latest Alerts
+                </h5>
+                <button class="text-[12px]" onclick={exportAlertsToPDF}>Export</button>
+
             </div>
             <Listgroup items={alerts} class="border-0 dark:bg-transparent">
                 {#snippet children(alert)}
-                <div class="flex items-center space-x-4 py-2 rtl:space-x-reverse">
-                    <div class="min-w-0 flex-1">
-                        <p class="truncate text-sm font-medium text-gray-900 dark:text-white">{alert.title}</p>
-                        <p class="truncate text-sm text-gray-500 dark:text-gray-499">{alert.description}</p>
+                    <div
+                        class="flex items-center space-x-4 py-2 rtl:space-x-reverse"
+                    >
+                        <div class="min-w-0 flex-1">
+                            <p
+                                class="truncate text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                {alert.title}
+                            </p>
+                            <p
+                                class="truncate text-sm text-gray-500 dark:text-gray-499"
+                            >
+                                {alert.description}
+                            </p>
+                        </div>
+                        <div
+                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                        >
+                            <SeverityBadge severity={alert.severity}
+                            ></SeverityBadge>
+                        </div>
                     </div>
-                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        <SeverityBadge severity={alert.severity}></SeverityBadge>
-                    </div>
-                </div>
                 {/snippet}
             </Listgroup>
         </Card>
@@ -100,15 +171,18 @@
 
         {#if isOwner}
             <Button onclick={() => (settingsModal = true)}>Settings</Button>
-            <GroupSettings bind:settingsModal users={data.users} owner={data.owner}></GroupSettings>
-            <Button onclick={() => (deleteGroupForm = true)}>Delete group</Button>
+            <GroupSettings
+                bind:settingsModal
+                users={data.users}
+                owner={data.owner}
+            ></GroupSettings>
+            <Button onclick={() => (deleteGroupForm = true)}
+                >Delete group</Button
+            >
             <DeleteGroupBtn bind:deleteGroupForm></DeleteGroupBtn>
         {/if}
-
     </div>
 </div>
 
-
 <style>
-
 </style>
